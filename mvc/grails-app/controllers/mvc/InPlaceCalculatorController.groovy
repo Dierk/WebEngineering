929dfef9405e0ddbf03572d174ce2953910aefb3
Dierk Koenig <dierk.koenig@canoo.com>
2017-03-10 19:39:52 +0100

    result alignment in view, generic field error handling

--- mvc/grails-app/controllers/mvc/InPlaceCalculatorController.groovy
+++ mvc/grails-app/controllers/mvc/InPlaceCalculatorController.groovy
@@ -4,17 +4,19 @@ import grails.validation.Validateable
 
 class InPlaceCalculatorController {
 
-    def calc(CalculatorModel model) {
-        model.en     = Math.round(model.en   * 10) / 10
-        model.exam   = Math.round(model.exam * 10) / 10
-        model.result = Math.round((model.en + model.exam) / 2)
-        if (0.0 == model.en) {
-            model.result = "Cannot calculate. En value was invalid."
+    def calc(CalculatorModel calcModel) {
+        calcModel.en     = Math.round(calcModel.en   * 10) / 10
+        calcModel.exam   = Math.round(calcModel.exam * 10) / 10
+        calcModel.result = Math.round((calcModel.en + calcModel.exam) / 2)
+        if (calcModel.errors.fieldErrors.any {it.field == "en"}) {
+            calcModel.result = "Cannot calculate. En value was invalid."
+            calcModel.en_error = "error"
+            calcModel.en_error_message = "value '$calcModel.en' is not valid, must be between 1.0 and 6.0."
         }
-        if (0.0 == model.exam) {
-            model.result = "Cannot calculate. Exam value was invalid."
+        if (0.0 == calcModel.exam) {
+            calcModel.result = "Cannot calculate. Exam value was invalid."
         }
-        render view:'calc', model: [model:model]
+        render view:'calc', model: [calculatorInstance: calcModel]
     }
 
 
@@ -25,9 +27,12 @@ class CalculatorModel implements Validateable {
     double exam   = 0.0
     String result = ""
 
+    String en_error = ""
+    String en_error_message = ""
+
     static constraints = {
-        en   min:1d, max:6d, matches: /\d\.\d/
-        exam min:1d, max:6d, matches: /\d\.\d/
+        en   min:1d, max:6d, scale:1
+        exam min:1d, max:6d, scale:1
         result nullable:true
     }
 }
--- mvc/grails-app/views/inPlaceCalculator/calc.gsp
+++ mvc/grails-app/views/inPlaceCalculator/calc.gsp
@@ -8,7 +8,21 @@
   <style>
     label {
       display: block;
+      float:left;
+      clear:left;
       width: 7em;
+    }
+    input {
+      display:block;
+      float:left;
+      clear:right;
+    }
+    .error {
+      border-color: red;
+    }
+    .padded {
+      padding: 1em;
+      margin: 0px;
     }
   </style>
 </head>
@@ -19,29 +33,33 @@
 
 
 <form action="/inPlaceCalculator/calc" method="get">
-  <fieldset class="form">
+  <fieldset class="form padded">
     <div>
       <label for='en'>En</label>
-      <input type="number decimal" name="en" value="${model.en}"
-             required="true" min="1.0" max="6.0" id="en"/>
+      <input type="number decimal" name="en" value="${calculatorInstance.en}"
+             required="true" min="1.0" max="6.0" id="en"
+             class="${calculatorInstance.en_error}"
+             title="${calculatorInstance.en_error_message}"
+      />
     </div>
 
     <div>
       <label for='exam'>Exam</label>
-      <input type="number decimal" name="exam" value="${model.exam}"
+      <input type="number decimal" name="exam" value="${calculatorInstance.exam}"
              required="true" min="1.0" max="6.0" id="exam"/>
     </div>
 
     <div>
+      <label>&nbsp;</label>
       <input type="submit" value="Calculate"/>
     </div>
   </fieldset>
 </form>
 
 
-<div>
+<div class="padded">
   <label>Result</label>
-  <output>${model.result}</output>
+  <output>${calculatorInstance.result}</output>
 </div>
 
 
