855bdd4043f964c3e2fd930296067b705b74aa0b
Dierk Koenig <dierk.koenig@canoo.com>
2017-03-19 12:35:27 +0100

    more fine-granular TDD

--- mvc/src/integration-test/groovy/mvc/InPlaceCalculatorSpec.groovy
+++ mvc/src/integration-test/groovy/mvc/InPlaceCalculatorSpec.groovy
@@ -42,6 +42,8 @@ class InPlaceCalculatorSpec extends GebSpec {
             $("#exam").attr('class') == ""
     }
 
+// TODO: un-comment the commented lines below and see them failing, then make them pass
+
     void "Invalid input is handled in-place by JS without submission"() {
         given: "a valid state"
             browser.driver.javascriptEnabled = true
@@ -53,13 +55,14 @@ class InPlaceCalculatorSpec extends GebSpec {
         then: "we should have a clean, valid state to start from"
             $("#en").attr('class') == ""
         when: "we enter some invalid value _without_ submitting"
-            def message = withAlert {
+//            def message = withAlert {
                 $("form").en = 0.9
-            }
+//            }
         then: "the in-place JS logic should kick in"
-            $("#en").attr('class') == "error"
-            message == "en value needs to be at least 1.0"
-            $("#en").focused
+//            $("#en").attr('class') == "error"
+//            message == "en value needs to be at least 1.0"
+//            $("#en").focused
     }
+
 
 }
