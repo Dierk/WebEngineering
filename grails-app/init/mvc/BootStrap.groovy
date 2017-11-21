c1d612140baf478a58c64e6f9d32914248a564b3
Dierk Koenig <dierk.koenig@canoo.com>
2017-11-21 09:29:54 +0100

    update GradedExercise.pdf to HS17 dates

--- grails-app/init/mvc/BootStrap.groovy
+++ grails-app/init/mvc/BootStrap.groovy
@@ -9,6 +9,16 @@ class BootStrap {
             return
         }
 
+        Room windowRoom = new Room(name: "1.333", max: 32).save(failOnError: true)
+        Room cornerRoom = new Room(name: "1.331", max: 42).save(failOnError: true)
+
+        Person dierk = new Person(firstName: "Dierk", lastName: "König", email:"dierk.koenig@fhnw.ch").save(failOnError:true)
+
+        Date today = new Date().clearTime();
+        new Booking(booker: dierk, room: windowRoom, date: today - 1, slot: "08:00 - 11:00").save(failOnError: true)
+        new Booking(booker: dierk, room: windowRoom, date: today, slot: "08:00 - 11:00").save(failOnError: true)
+        new Booking(booker: dierk, room: windowRoom, date: today + 1, slot: "08:00 - 11:00").save(failOnError: true)
+        new Booking(booker: dierk, room: cornerRoom, date: today , slot: "12:00 - 15:00").save(failOnError: true)
 
     }
 
